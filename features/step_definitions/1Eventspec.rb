4c23fcaaef23b05f17c3188c21833936677e76e4
Brian-Almendras-SC <brian.almendras@ucb.edu.bo>
2022-11-28 16:58:23 -0400

    first intent to create automation test of one event

--- features/step_definitions/1Eventspec.rb
+++ features/step_definitions/1Eventspec.rb
@@ -4,12 +4,12 @@ Given('I click in the option {string}') do |string|
   end
   
   When('I find {string}') do |string|
-    xpath = '//*[@id="root"]/div[2]/header/div[2]/div/button[3]'
-    find(:xpath, xpath).click
+    page.find('span', text: string,match: :first).hover
+    sleep(5)
   end
   
   Then('I click over Detalles') do
-    pending # Write code here that turns the phrase above into concrete actions
+    puts "5"
   end
   
   Then('Show details page about this event') do
